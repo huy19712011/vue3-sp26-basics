cc1334773c9cc493d578bd6a0604cc38184cdcd0
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-12-30 22:29:25 +0700

    set up vue3 project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h2>About</h2>
+  </div>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div class="home">
+    <h2>Home</h2>
+  </div>
+</template>
